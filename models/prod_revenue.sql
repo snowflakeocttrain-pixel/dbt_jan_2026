9eacd9639d7b69c20018365057e7b2d20d63a89d
vinod kothakonda <snowflakeocttrain@gmail.com>
2026-01-13 15:00:25 +0000

    day2 and day3

--- models/prod_revenue.sql
+++ models/prod_revenue.sql
@@ -1,3 +1,5 @@
+{{ config (materialized='table',alias='revenue_tbl',transient=false)}}
+
 select name,sum(unit_price*units_sold) as revenue 
 from raw_data.products p inner join raw_data.sales s
 on p.prod_id=s.prod_id
